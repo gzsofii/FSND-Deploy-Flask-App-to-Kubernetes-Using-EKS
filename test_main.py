87f29c4ccbfa575c702b1068bcc301fae053f5c7
gzsofii <gal.zsofi@gmail.com>
2021-08-08 01:09:02 +0200

    make it work again

--- test_main.py
+++ test_main.py
@@ -28,7 +28,7 @@ def test_health(client):
     assert response.status_code == 200
     assert response.json == 'Healthy'
     
-    assert False # to make it fail
+    #assert False # to make it fail
 
 def test_auth(client):
     body = {'email': EMAIL,
